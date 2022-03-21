6f30d1209c64e2d600ae2ab959889ae0ec2ae0fc
unknown <SQLI@OUJ1WR4NK2.smb.sqli.com>
2022-03-21 18:32:03 +0100

    first commit

---
+++ Vidly1/Vidly1/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+namespace Vidly1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movie = new Movie() { Name = "Frozen" };
+            return View(movie);
+        }
+    }
+}
